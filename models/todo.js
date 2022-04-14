c80af8e1bd69a55fdf352d3a0f038a1baf3be076
Wu Justin <justin.cathay@gmail.com>
2022-04-15 01:10:46 +0800

    add models/todo

---
+++ models/todo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema(
+  {
+    _id: String,
+    title: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false //不回傳前端
+    }
+  },
+  //預設值物件
+  {
+    versionkey: false,
+    //時間戳記
+    //timestamps: true,
+
+    //自訂集合,最後不想強制加上s
+    //collection: 'todo' //這邊自訂名稱
+  }
+)
+
+const Todo = mongoose.model('Todo',todoSchema);
+
+model.exports = Todo
